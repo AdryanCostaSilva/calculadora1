0ad0d22aaa735f5c114a5e699965823d69f31409
AdryanCostaSilva <163481660+AdryanCostaSilva@users.noreply.github.com>
2024-04-03 10:33:44 -0300

    Update calculadora.py
    Calculos com vários números

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,14 @@
-num1 = int(input("Digite o primeiro número: "))
-num2 = int(input("Digite o segundo número: "))
-print("Opções: \n [1] SOMAR\n [2] SUBTRAIR\n [3] MULTIPLICAR\n [4] DIVIDIR\n [5]SAIR")
 opcao = ''
 while opcao != 5:
+    print("Opções: \n [1] SOMAR\n [2] SUBTRAIR\n [3] MULTIPLICAR\n [4] DIVIDIR\n [5]SAIR")
     opcao = int(input("Sua escolha: "))
+    if opcao == 5:
+        break
+    elif opcao < 1 or opcao > 5:
+        print("Opção inválida tente novamente")
+        continue
+    num1 = int(input("Digite o primeiro número: "))
+    num2 = int(input("Digite o segundo número: "))
     if opcao == 1:
         print(f"A soma entre {num1} e {num2} é {num1 + num2}")
     elif opcao == 2:
@@ -12,7 +17,4 @@ while opcao != 5:
         print(f"A multiplicação entre {num1} e {num2} é {num1 * num2}")
     elif opcao == 4:
         print(f"A divisão entre {num1} e {num2} é {num1 / num2} ")
-    elif opcao < 1 or opcao > 5:
-        print("Opção inválida tente novamente")
-        continue
 print(f"Você saiu do programa, até mais!")
